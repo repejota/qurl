0e5568e6bda41c382f5224f845e4a27a4ac39b08
Raül Pérez <repejota@gmail.com>
2017-09-10 21:10:18 +0200

    Fetch url data

--- routes/query.go
+++ routes/query.go
@@ -19,16 +19,26 @@ type Response struct {
 
 // Query fetch an URL and returns JSON with the data obtained.
 func Query(c echo.Context) error {
+	u := c.QueryParam("url")
+
 	result := &Response{}
+	result.URL = u
 	result.Status = http.StatusOK
 
 	// Validate URL
-	u := c.QueryParam("url")
 	_, err := url.ParseRequestURI(u)
 	if err != nil {
 		result.Status = http.StatusBadRequest
+		return c.JSON(result.Status, result)
 	}
 
-	result.URL = u
+	// Fetch URL content
+	response, err := http.Get(u)
+	if err != nil {
+		result.Status = http.StatusInternalServerError
+		return c.JSON(result.Status, result)
+	}
+	defer response.Body.Close()
+
 	return c.JSON(result.Status, result)
 }
