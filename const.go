e48d0a79542c0a8a781033df194a9fba1042d0b8
Raül Pérez <repejota@gmail.com>
2017-09-10 11:50:20 +0200

    package doc and constants

---
+++ const.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The qurl Authors. All rights reserved.
+
+package qurl
+
+const (
+	// DefaultServerAddress is the default hostname where the server is going to
+	// be started and listening.
+	DefaultServerAddress = "localhost"
+	// DefaultServerPort is the default port number where the server is going to
+	// be started and listening.
+	DefaultServerPort = "8080"
+)
