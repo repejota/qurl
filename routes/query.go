7487970b5d49691853c41c89fe93c744e1b0e46a
Raül Pérez <repejota@gmail.com>
2017-09-23 12:45:10 +0200

    Refactor core

--- routes/query.go
+++ routes/query.go
@@ -6,9 +6,7 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
-	"net/url"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/repejota/qurl"
 )
 
@@ -16,46 +14,20 @@ import (
 func Query(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 
-	u := queryParams.Get("url")
-
-	result := qurl.NewResponse()
-	result.URL = u
-	result.Status = http.StatusOK
-
-	// Validate URL
-	_, err := url.ParseRequestURI(u)
+	qurl := qurl.NewQURL()
+	err := qurl.SetURL(queryParams.Get("url"))
 	if err != nil {
 		http.Error(w, "INVALID_URL", http.StatusBadRequest)
 		return
 	}
 
-	// Fetch URL content
-	response, err := http.Get(u)
-	if err != nil {
-		http.Error(w, "INTERNAL_ERROR", http.StatusInternalServerError)
-		return
-	}
-	defer func() {
-		err := response.Body.Close()
-		if err != nil {
-			http.Error(w, "INTERNAL_ERROR", http.StatusInternalServerError)
-			return
-		}
-	}()
-
-	err = processHeaders(queryParams, response, result)
+	response, err := qurl.Query(queryParams)
 	if err != nil {
 		http.Error(w, "INTERNAL_ERROR", http.StatusInternalServerError)
 		return
 	}
 
-	err = processSelectors(queryParams, response, result)
-	if err != nil {
-		http.Error(w, "INTERNAL_ERROR", http.StatusInternalServerError)
-		return
-	}
-
-	resultJSON, err := json.Marshal(result)
+	responseJSON, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, "INTERNAL_ERROR", http.StatusInternalServerError)
 		return
@@ -63,48 +35,5 @@ func Query(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resultJSON)
-}
-
-// processHeaders ...
-func processHeaders(queryParams url.Values, response *http.Response, result *qurl.Response) error {
-	for _, v := range queryParams["header"] {
-		result.Headers[v] = response.Header[v]
-	}
-	return nil
-}
-
-// processSelectors ...
-func processSelectors(queryParams url.Values, response *http.Response, result *qurl.Response) error {
-	// Build a DOM from response content
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		return err
-	}
-
-	// Query the DOM with all selectors
-	for _, v := range queryParams["selector"] {
-
-		// Process matching nodes
-		doc.Find(v).Each(func(index int, selection *goquery.Selection) {
-
-			// Node text
-			element := &qurl.Element{
-				Text: selection.Text(),
-			}
-
-			// Node attributes
-			for _, v := range selection.Nodes[0].Attr {
-				attr := &qurl.Attribute{
-					Key:   v.Key,
-					Value: v.Val,
-				}
-				element.Attributes = append(element.Attributes, attr)
-			}
-
-			result.Selectors[v] = append(result.Selectors[v], element)
-		})
-	}
-
-	return nil
+	w.Write(responseJSON)
 }
