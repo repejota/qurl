1865ab5973f29ee8c60d5cdc3131b41eb09ddd21
Raül Pérez <repejota@gmail.com>
2017-09-24 02:20:42 +0200

    Test SetUURL

--- qurl_test.go
+++ qurl_test.go
@@ -10,4 +10,31 @@ func TestNewQURL(t *testing.T) {
 	if qurl.URL != "" {
 		t.Fatalf("QURL url expected to be %s but got %s", expectedURL, qurl.URL)
 	}
+	if qurl.Response.URL != "" {
+		t.Fatalf("QURL Response url expected to be %s but got %s", expectedURL, qurl.Response.URL)
+	}
+}
+
+func TestSetURL(t *testing.T) {
+	expectedURL := "https://www.example.com"
+	qurl := NewQURL()
+	err := qurl.SetURL(expectedURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if qurl.URL != expectedURL {
+		t.Fatalf("QURL url expected to be %s but got %s", expectedURL, qurl.URL)
+	}
+	if qurl.Response.URL != expectedURL {
+		t.Fatalf("QURL Response url expected to be %s but got %s", expectedURL, qurl.Response.URL)
+	}
+}
+
+func TestSetURLInvalid(t *testing.T) {
+	expectedURLInvalid := "foo"
+	qurl := NewQURL()
+	err := qurl.SetURL(expectedURLInvalid)
+	if err.Error() != "parse foo: invalid URI for request" {
+		t.Fatal(err)
+	}
 }
