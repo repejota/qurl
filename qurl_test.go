233544f0d7a7c1fe156bfdaf340b118cf24e6669
Raül Pérez <repejota@gmail.com>
2017-09-24 02:13:47 +0200

    More tests

--- qurl_test.go
+++ qurl_test.go
@@ -1,3 +1,13 @@
 // Copyright 2017 The qurl Authors. All rights reserved.
 
 package qurl
+
+import "testing"
+
+func TestNewQURL(t *testing.T) {
+	expectedURL := ""
+	qurl := NewQURL()
+	if qurl.URL != "" {
+		t.Fatalf("QURL url expected to be %s but got %s", expectedURL, qurl.URL)
+	}
+}
