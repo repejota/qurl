4e00c1e0eb26a6d69fe9e4e882015e1a0fcd031d
Raül Pérez <repejota@gmail.com>
2017-09-26 12:55:33 +0200

    More tests

--- qurl_basic_selectors_id_test.go
+++ qurl_basic_selectors_id_test.go
@@ -35,30 +35,32 @@ func TestIDSelectorNotPresent(t *testing.T) {
 	}
 }
 
-/*
 func TestIDSelectorPresent(t *testing.T) {
-	req, err := http.NewRequest("GET", "/q?url=http://localhost:6060&selector=%23idname", nil)
+	targetURL := "https://www.example.com"
+	requestURL := fmt.Sprintf("/q?url=%s&selector=.content", targetURL)
+	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	rec := httptest.NewRecorder()
-	handler := http.HandlerFunc(Query)
-
-	handler.ServeHTTP(rec, req)
-
-	var response Response
-	err = json.Unmarshal(rec.Body.Bytes(), &response)
+	q := &QURL{}
+	freq := &FakeRequest{
+		ExpectedBody:       `<p class="content">Page content</title>`,
+		ExpectedStatusCode: http.StatusOK,
+	}
+	response, err := q.Query(freq, req.URL.Query())
 	if err != nil {
-		t.Fatalf("Unmarshaling response failed %v", err)
+		t.Fatal(err)
 	}
-
-	if len(response.Selectors["#idname"]) != 1 {
-		t.Fatalf("Response selector '#idname' expected to have one element but got '%v'", response.Selectors["#idname"])
+	if response.Status != http.StatusOK {
+		t.Errorf("response status expected to be %d but got %d", http.StatusOK, response.Status)
 	}
-
-	if response.Selectors["#idname"][0].Text != "selector id content" {
-		t.Fatalf("Response selector '#idname' expected to be 'selector id content' but got '%v'", response.Selectors["#idname"][0])
+	if response.URL != targetURL {
+		t.Errorf("response url expected to be %s but got %s", targetURL, response.URL)
+	}
+	if len(response.Selectors[".content"]) != 1 {
+		t.Fatalf("Response selector '.content' expected to have one element but got '%v'", len(response.Selectors[".content"]))
+	}
+	if response.Selectors[".content"][0].Text != "Page content" {
+		t.Fatalf("Response selector '.content' expected to be %s but got %s", "Page content", response.Selectors[".content"][0].Text)
 	}
 }
-*/
